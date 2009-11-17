d0a3a16bd6209ea8498626b802ed5cbbc5d4869b
Christopher Hlubek <hlubek@networkteam.com>
2009-11-17 11:31:30 +0100

    Some few enhancements

--- app/models/bayes.rb
+++ app/models/bayes.rb
@@ -17,30 +17,27 @@ class Bayes
     end
     def classify(text)
       scores = {}
+      total_texts = Ohm.redis.get("total_texts").to_f
       get_classes.each do |klass|
-        scores[klass] = (prob_of_text_given_a_class(text, klass) * prob_of_class(klass))
+        class_count = Ohm.redis.get("#{klass}").to_f
+        scores[klass] = (prob_of_text_given_a_class(text, klass, class_count) * prob_of_class(klass, class_count, total_texts))
       end
       scores.sort {|a, b| b[1] <=> a[1]}[0]
     end
-    def prob_of_text_given_a_class(text, klass)
+    def prob_of_text_given_a_class(text, klass, class_count)
       words(text).inject(1.0) do |sum, word|
-        prob = prob_of_word_given_a_class(word, klass)
+        prob = prob_of_word_given_a_class(word, klass, class_count)
         prob = assumed_probability if prob == 0
         sum *= prob
       end
     end
-    def prob_of_word_given_a_class(word, klass)
-      class_count = Ohm.redis.get("#{klass}").to_f
+    def prob_of_word_given_a_class(word, klass, class_count)
       return 0.5 if class_count == 0.0
       class_word_count = Ohm.redis.get("#{klass}:#{word}").to_f
       class_word_count / class_count
     end
-    def prob_of_class(klass)
-      class_count = Ohm.redis.get("#{klass}").to_f
+    def prob_of_class(klass, class_count, total_texts)
       class_count / total_texts
-    end
-    def total_texts
-      Ohm.redis.get("total_texts").to_f
     end
     def assumed_probability
       0.5 / (total_texts / 2.0)
--- app/routes/site.rb
+++ app/routes/site.rb
@@ -1,16 +1,27 @@
 class Main
   get "/" do
+    @classes = Bayes.get_classes
     haml :home
   end
   get "/train" do
-    haml :train, :classes => Bayes.get_classes
+    haml :train
+  end
+  get "/train/:klass" do
+    @klass = params[:klass]
+    haml :train
   end
   post '/train' do
     klass = params[:klass]
     Bayes.add_class(klass)
     Bayes.train(klass, params[:text])
     
-    "Rebayes knows something more about #{klass}: #{Ohm.redis.get klass}"
+    redirect "/train_success/#{klass}"
+  end
+  get '/train_success/:klass' do
+    @klass = params[:klass]
+    @klass_count = Ohm.redis.get @klass
+
+    haml :train_success
   end
   get '/classify' do
     haml :classify
